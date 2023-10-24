9a02fcf3d60ee08b8d75dac761c23c87c246460d
Bayu <91533373+AcydAce@users.noreply.github.com>
2023-10-24 08:48:48 +0700

    added comment

--- AI_Forums_Answer.pl
+++ AI_Forums_Answer.pl
@@ -1,6 +1,6 @@
-is_it_right_triangle(Angle1, Angle2) :-
-    Angle1 + Angle2 =:= 90.
-
+is_it_right_triangle(Angle1, Angle2) :-    %it checks 2 angles whether it is 90,
+    Angle1 + Angle2 =:= 90.                %if it is 90, it the other angle will be considered as 90
+                                        
 triangle(Angle1, Angle2, Angle3) :-
     is_it_right_triangle(Angle1, Angle2),
     Angle3 is 180 - (Angle1 + Angle2),
